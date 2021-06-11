00912cc76ce5edb72b91c5a9f1e93b84e26fba89
goodmean <zizigoo12@gmail.com>
2021-06-11 09:46:35 +0900

    0611

---
+++ 0611.py
@@ -0,0 +1,14 @@
+print("Hello World!") #문자는 쌍따옴표
+print(135+223) #숫자는 그냥쓰기
+x = 3 #변수
+print(x) #이후부터 x는 3
+print("x : 10") #쌍따옴표 안 x는 문자x
+print("x = {}".format(1))
+print("x = {}".format(x))
+a = 10
+a = 5
+print(a)
+a = 10
+b = 100
+print("a : {}".format(a))
+print("b : {}".format(b))
